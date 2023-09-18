371847093ec306ab6dc06439b6f06e5299b0275b
Supun_h <supunh.kas@gmail.com>
2023-09-18 17:22:38 +0530

    add controllers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './controllers/auth/auth.module';
+import { RequestModule } from './controllers/request/request.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, RequestModule],
   controllers: [AppController],
   providers: [AppService],
 })
